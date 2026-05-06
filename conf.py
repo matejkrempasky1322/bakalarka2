4e11beb7f5406bd6a6cd02fef05d8fdd56ddd741
matej <bobor668@gmail.com>
2026-05-06 21:20:46 +0200

    Theme 4

--- conf.py
+++ conf.py
@@ -16,13 +16,10 @@ release = '1.0'
 
 extensions = [
     'nbsphinx',
-    'sphinx_book_theme',
 ]
 
 language = 'sk'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+exclude_patterns = ['_build', '**/_build/**']
